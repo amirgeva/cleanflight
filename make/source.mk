2d712d66b9722d97fd3af4c94b18fe8704c8eafc
Amir Geva <amirgeva@cs.technion.ac.il>
2017-09-02 11:46:31 +0300

    Added isl.c to source list

--- make/source.mk
+++ make/source.mk
@@ -159,6 +159,7 @@ FC_SRC = \
             telemetry/smartport.c \
             telemetry/ltm.c \
             telemetry/mavlink.c \
+            telemetry/isl.c \
             telemetry/ibus.c \
             telemetry/ibus_shared.c \
             sensors/esc_sensor.c \
